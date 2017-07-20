555b98f817910e13cccc816a25516a19ae406ac8
赤地岳 <akatigaku@gmail.com>
2017-07-20 15:27:57 +0900

    write bonus drink function with test

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,15 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total = amount
+    bonus = 0
+    while amount > 2 do
+      # 3blank cans can bonus 1 bottle
+      amount -= 2
+      # count bonus bottle amount
+      bonus += 1
+    end
+    return total += bonus
   end
 end
+
+puts BonusDrink.total_count_for(100)
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
-describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+describe 'BonusDrink' do
+  it 'when amount is 0, total should be 0' do
+    expect(BonusDrink.total_count_for(0)).to eq 0
+  end
+
+  it 'when amount is 1, total should be 1' do
+    expect(BonusDrink.total_count_for(1)).to eq 1
+  end
+
+  it 'when amount is 3, total should be 4' do
+    expect(BonusDrink.total_count_for(3)).to eq 4
+  end
+
+  it 'when amount is 11, total should be 16' do
+    expect(BonusDrink.total_count_for(11)).to eq 16
+  end
 end
